7807d0e07fcf85ee0e057becc7b0cc9a8d553678
anyagrebenko <a.grebenko@bk.ru>
2018-02-14 00:56:47 +0300

    orign

--- app/src/main/java/com/example/user/vg/MainActivity.java
+++ app/src/main/java/com/example/user/vg/MainActivity.java
@@ -3,10 +3,17 @@ package com.example.user.vg;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -14,16 +21,21 @@ public class MainActivity extends AppCompatActivity{
 //    Г Л А В Н Ы Й
 //    Э К Р А Н
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
     }
 
+//    public void loadWebPage(View v){
+//        Intent intent = new Intent(this, WebActivity.class);
+//        startActivity(intent);
+//    }
+
     public void OnBtn (View v){
-        if(v.getId() == R.id.btngo2){
+        if(v.getId() == R.id.imagebtnvol){
             Intent i = new Intent(MainActivity.this, SpeechActivity.class);
             startActivity(i);
         }
@@ -32,7 +44,7 @@ public class MainActivity extends AppCompatActivity{
     }
 
     public void OnGo (View v){
-        if (v.getId() == R.id.btngo){
+        if (v.getId() == R.id.imagebtncalc){
             Intent i = new Intent(MainActivity.this, CalculatorActivity.class);
             startActivity(i);
         }
@@ -41,7 +53,7 @@ public class MainActivity extends AppCompatActivity{
     }
 
     public void OnAbout (View v){
-        if (v.getId() == R.id.btngo3){
+        if (v.getId() == R.id.imagebtnabout){
             Intent i = new Intent(MainActivity.this, AboutActivity.class);
             startActivity(i);
         }
